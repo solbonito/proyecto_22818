66dc8dbb7958eaee8d76c2d63575a609e4b15cce
solbonito <solbonito2010@gmail.com>
2022-09-26 21:12:49 -0300

    Se actualiza urls.py y viesw.py

--- cac/views.py
+++ cac/views.py
@@ -1,3 +1,18 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def hola_mundo(request):
+    return HttpResponse('Hola Mundo Django 🦄')
+    
+def saludar(request, nombre = 'Daniela'):
+    return HttpResponse(f"""
+        <h1>Hola mundo Django - un gusto {nombre}</h1>
+    """)
+
+def ver_proyectos(request, anio, mes):
+    return HttpResponse(f"""
+        <h1>Proyectos del {mes}/{anio}</h1>
+        <p>Listado de proyectos</p>
+    """)
